06066f02d3c509e1ff5a9e48de6b6b80f04ad3cb
Badal Aryal <badal.aryal@gmail.com>
2024-03-27 12:14:40 +0545

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Badal  Aryal on 27/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+    
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+    
+    }
+
+}

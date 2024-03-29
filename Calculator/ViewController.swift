0b5a5fb8f6593332c8e2651ca90014be4379fb21
Badal Aryal <badal.aryal@gmail.com>
2024-03-29 10:49:04 +0545

    Add more functionality to our calculator.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -24,7 +24,17 @@ class ViewController: UIViewController {
             fatalError("Cannot convert display label text to a Double.")
         }
         
-       
+        if let calcMethod = sender.currentTitle{
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            }
+            else if calcMethod == "AC"{
+                displayLabel.text = "0"
+            }
+            else if calcMethod == "%"{
+                displayLabel.text = String(number / 100)
+            }
+        }
         
     }
 
